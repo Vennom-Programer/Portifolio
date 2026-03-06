5b704aa348b52043e707242280936c2cd0069772
Kauã Vaz <kauavaz40@gmail.com>
2026-03-06 01:28:13 -0300

    Add footer copyright notice and implement image rotation on click

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,6 @@
 const girarImagem = document.getElementsByClassName("imagem");
+for (let i = 0; i < girarImagem.length; i++) {
+    girarImagem[i].addEventListener("click", function () {
+        this.classList.toggle("girar");
+    });
+}
