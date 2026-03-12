4efc0aa935a469ef2f9fb43e8ca0974077a0b3a8
Kauã Vaz <kauavaz40@gmail.com>
2026-03-12 20:03:35 -0300

    rotação de imagem

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
+let iamgem = 0;
+
+
 const girarImagem = document.getElementsByClassName("imagem");
 for (let i = 0; i < girarImagem.length; i++) {
     girarImagem[i].addEventListener("click", function () {
-        this.classList.toggle("girar");
+        iamgem = (iamgem + 1) % 4;
+        this.style.transform = "rotate(" + (iamgem * 360) + "deg)";
     });
 }
